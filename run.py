e6b3c864dbe8f2c0bae02d37bcf7d928bc853c26
aymen95-code <aymengk94@gmail.com>
2020-12-11 20:51:28 +0100

    final commit

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 from Zoom import create_app
 
 app = create_app()
-if __name__ == "__main__":
-    app.run(debug=True)
+
+# it's better to use flask commands
+""" if __name__ == "__main__":
+    app.run(debug=True) """
